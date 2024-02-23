d47b0c5d79c5a2c468e64988e883ca0e2ea1f11b
Srikanth <srikanthkarthi2003@gmail.com>
2024-02-23 09:55:52 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import express  from "express";
 
 const app= express();
-
-
 const port=process.env.PORT || 8000;
 app.get("/",(req, res)=>
 {
-res.send('<h1>hello</h1>')
+res.send('<h1>hello shanjai</h1>')
 })
 
 app.listen(port,()=>
